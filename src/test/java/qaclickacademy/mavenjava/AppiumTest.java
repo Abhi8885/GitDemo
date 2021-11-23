f566b90a1c332a5340aa6fd51c5c1fe72464457e
Abhilash <dadigeabhilash@gmail.com>
2021-11-23 16:19:09 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NagtiveAppAndroid()
+	{
+		System.out.println("NagtiveAppAndroid");
+	}
+	@Test
+	public void IosApps()
+	{
+		System.out.println("IosApps");
+	}
+}
